366ce958c3cde3fa8286b8d5e4ab538a0b94d51a
bsmith-dotcom <brandon.smith@asurion.com>
2025-01-24 15:25:15 -0600

    Updates

--- src/ components/MoviesGrid.js
+++ src/ components/MoviesGrid.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
 import '../styles.css';
 
 export default function MoviesGrid()
 {
-    return(
-        <div>
+    const [movies, setMovies] = useState([]);
 
+    //Load the movies
+    useEffect(() => {
+      fetch("movies.json")
+      .then(response => response.json())
+      .then(data => setMovies(data));
+
+    }, []);
+    
+    return(
+        <div className="movies-grid">
+            {
+                movies.map(
+                    movie => (
+                        <div key={movie.id} className='movie-card'>
+                            <img src={`images/${movie.image}`} alt={movie.title}/>
+                            <div className='movie-card-info'>
+                                <h3 className='movie-card-title'>{movie.title}</h3>
+                                <p className="movie-card-genre">{movie.genre}</p>
+                                <p className="movie-card-rating">{movie.rating}</p>
+                            </div>
+                        </div>
+                    )
+                )
+            }   
+            <p>{movies.length} results</p>         
         </div>
     );
 }
